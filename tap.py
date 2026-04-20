bfb8e73a8ae989f93d1becb75b3e7b9cde132f61
optimusprime <tamerlan2012memmedli@gmail.com>
2026-04-20 13:56:17 +0400

    Add files via upload

---
+++ tap.py
@@ -0,0 +1,13 @@
+import random
+
+def count_numbers():
+    numbers = [random.randint(1, 100) for __ in range(10)]
+    print("List:", numbers)
+    
+
+    count = sum(1 for num in numbers if num % 3 == 0 and num % 5 != 0)
+    
+    return count
+
+result = count_numbers()
+print(result)
